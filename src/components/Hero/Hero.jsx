f48be3fde3293fe6689bb9bf4abdb673142fa352
Evgeniya Plumey <e.plumey@outlook.com>
2024-04-18 22:15:02 +0200

    page d'accueil

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,10 +7,10 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Ada</h1>
+        <h1 className={styles.title}>Hello, </h1>
         <p className={styles.description}>
-          I'm a full-stack developer with 5 years of experience using React and
-          NodeJS. Reach out if you'd like to learn more!
+        Bienvenue sur mon portfolio ! Je suis une développeuse front-end récemment formé, passionné par la création d'expériences web innovantes. 
+        Découvrez mes projets et contactez-moi pour discuter de collaborations excitantes!
         </p>
         <a href="mailto:myemail@email.com" className={styles.contactBtn}>
           Contact Me

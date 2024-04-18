94756001b86f2c75158eec258d7eadcd14c4716a
Evgeniya Plumey <e.plumey@outlook.com>
2024-04-18 20:30:42 +0200

    hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import styles from "./App.module.css";
 import { Navbar } from "./components/Navbar/Navbar";
+import { Hero } from "./components/Hero/Hero";
 <Navbar />;
 
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className={styles.App}>
       <Navbar />
+      <Hero />
     </div>
   )
 }
